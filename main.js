40c23605a4d8a64f1b2bf3dceddc46f00cd071bc
schopenhua <shubenhuahua@outlook.com>
2022-03-31 16:40:22 +0800

    用switch循环打印data内容

--- main.js
+++ main.js
@@ -5,17 +5,21 @@ fs.readFile("./test.js", "utf8", function(err, data) {
         console.log("文件读取失败！");
         return;
     }
-    // console.log(typeof(data));
     data = data.trim();
     for (var i = 0; i < data.length; i++) {
-        if (data[i] === " ") {
-            console.log("-")
-        } else if (data[i] === '\n') {
-            console.log('\\n')
-        } else if (data[i] === "\"") {
-            console.log("\"")
-        } else {
-            console.log(data[i]);
+        switch (data[i]) {
+            case " ":
+                console.log("-");
+                break;
+            case "\n":
+                console.log("\\n");
+                break;
+            case "\"":
+                console.log("\"");
+                break;
+            default:
+                console.log(data[i]);
+                break;
         }
     }
 });
